e971b274b99fb5a80311872de25d0e8e3eb172b0
Dmitry Mikushin <dmitry@kernelgen.org>
2020-10-08 19:49:15 +0000

    Adding a Python example and getting it to work correctly

---
+++ examples/example.py
@@ -0,0 +1,38 @@
+import cutt
+import numpy as np
+import pycuda.autoinit
+import pycuda.driver as cuda
+import pycuda.gpuarray as gpuarray
+
+if __name__ == '__main__':
+    # Four dimensional tensor
+    # Transpose (31, 549, 2, 3) -> (3, 31, 2, 549)
+    dim = [ 31, 549, 2, 3 ]
+    permutation = [ 3, 0, 2, 1 ]
+
+    # .... input and output data is setup here ...
+    idata = np.random.randn(np.prod(dim)).astype(np.float64).reshape(dim)
+    idata_gpu = gpuarray.to_gpu(idata)
+    odata_gpu = gpuarray.empty(dim, dtype=np.float64)
+
+    option = 1
+
+    if option == 1:
+        # Option 1: Create plan on NULL stream and choose implementation based on heuristics
+        plan = cutt.cuTT(4, dim, permutation, None)
+    else:
+        # Option 2: Create plan on NULL stream and choose implementation based on performance measurements
+        plan = cutt.cuTT(4, dim, permutation, None, idata_gpu, odata_gpu)
+
+    # Execute plan
+    result = plan.execute(idata_gpu, odata_gpu)
+
+    # ... do stuff with your output and deallocate data ...
+    odata = odata_gpu.get()
+
+    print("idata.avg() = {}, odata.avg() = {}".format(np.average(idata), np.average(odata)))
+
+    # Note: make sure to delete the plan before the CUDA driver is deinitialized, as the
+    # plan contains internal GPU memory buffers, that shall otherwise fail to deallocate.
+    del plan
+
--- src/python/cutt.cpp
+++ src/python/cutt.cpp
@@ -2,8 +2,10 @@
 
 #include <pybind11/pybind11.h>
 #include <pybind11/numpy.h>
+#include <pybind11/stl.h>
 #include <memory>
 #include <mutex>
+#include <sstream>
 #include <string>
 #include <vector>
 
@@ -13,6 +15,29 @@ namespace {
 
 static std::once_flag cuttInitialized;
 
+const char* cuttErrorString(cuttResult result)
+{
+	const char* cuttSuccess = "Success";
+	const char* cuttInvalidPlan = "Invalid plan handle";
+	const char* cuttInvalidParameter = "Invalid input parameter";
+	const char* cuttInvalidDevice = "Execution tried on device different than where plan was created";
+	const char* cuttInternalError = "Internal error";
+	const char* cuttUndefinedError = "Undefined error";
+	const char* cuttUnknownError = "Unknown error";
+
+	switch (result)
+	{
+	case CUTT_SUCCESS: return cuttSuccess;
+	case CUTT_INVALID_PLAN: return cuttInvalidPlan;
+	case CUTT_INVALID_PARAMETER: return cuttInvalidParameter;
+	case CUTT_INVALID_DEVICE: return cuttInvalidDevice;
+	case CUTT_INTERNAL_ERROR: return cuttInternalError;
+	case CUTT_UNDEFINED_ERROR: return cuttUndefinedError;
+	}
+
+	return cuttUnknownError;
+}
+
 class cuTT
 {
 	cuttHandle plan;
@@ -20,8 +45,8 @@ class cuTT
 	bool planInitialized = false;
 
 	int rank;
-	const std::vector<int>& dim;
-	const std::vector<int>& permutation;
+	const std::vector<int> dim;
+	const std::vector<int> permutation;
 	cudaStream_t stream;
 
 public :
@@ -36,14 +61,23 @@ public :
 
 		// Defer cuttPlan to the first use, as we don't know the type yet.
 
-		py::object pycudadriver = py::module::import("pycuda.driver");
-                py::object pystream = pycudadriver.attr("stream");
+		py::object pycuda = py::module::import("pycuda");
+		py::object pystream = pycuda.attr("driver").attr("Stream");
 
-                if (!py::isinstance(stream_, pystream))
-                	throw std::invalid_argument(
-                        	"Stream argument must be a pycuda.driver.stream");
+		if (stream_.is_none())
+			stream = 0;
+		else
+		{
+			if (!py::isinstance(stream_, pystream))
+			{
+				std::stringstream ss;
+				ss << "Stream argument must be a pycuda.driver.stream, got: ";
+				ss << py::str(stream_);
+				throw std::invalid_argument(ss.str());
+			}
 
-                stream = (cudaStream_t)stream_.attr("handle_int").cast<intptr_t>();
+			stream = (cudaStream_t)stream_.attr("handle_int").cast<intptr_t>();
+		}
 	}
 
 	cuTT(int rank_, const std::vector<int>& dim_, const std::vector<int>& permutation_, const py::object& stream_,
@@ -51,35 +85,53 @@ public :
 		rank(rank_), dim(dim_), permutation(permutation_)
 	{
 		// This is only needed for the Umpire allocator's lifetime management:
-                // - if CUTT_HAS_UMPIRE is defined, will grab Umpire's allocator;
-                // - otherwise this is a no-op
-                std::call_once(cuttInitialized, [](){ cuttInitialize(); });
+		// - if CUTT_HAS_UMPIRE is defined, will grab Umpire's allocator;
+		// - otherwise this is a no-op
+		std::call_once(cuttInitialized, [](){ cuttInitialize(); });
 
 		py::object pycuda = py::module::import("pycuda");
 		py::object pygpuarray = pycuda.attr("gpuarray");
-		py::object pycudadriver = py::module::import("pycuda.driver");
-		py::object pystream = pycudadriver.attr("stream");
+		py::object pystream = pycuda.attr("driver").attr("Stream");
 
 		if (!py::isinstance(idata, pygpuarray))
-			throw std::invalid_argument(
-                                "Input array must be a pycuda.gpuarray");
+		{
+			std::stringstream ss;
+			ss << "Input array must be a pycuda.gpuarray, got: ";
+			ss << py::str(idata);
+			throw std::invalid_argument(ss.str());
+		}
 		if (!py::isinstance(odata, pygpuarray))
-                        throw std::invalid_argument(
-                                "Output array must be a pycuda.gpuarray");
-		if (!py::isinstance(stream_, pystream))
-                        throw std::invalid_argument(
-                                "Stream argument must be a pycuda.driver.stream");
+		{
+			std::stringstream ss;
+			ss << "Output array must be a pycuda.gpuarray, got: ";
+			ss << py::str(odata);
+			throw std::invalid_argument(ss.str());
+		}
+
+		if (stream_.is_none())
+			stream = 0;
+		else
+		{
+			if (!py::isinstance(stream_, pystream))
+			{
+				std::stringstream ss;
+				ss << "Stream argument must be a pycuda.driver.stream, got: ";
+				ss << py::str(stream_);
+				throw std::invalid_argument(ss.str());
+			}
+
+			stream = (cudaStream_t)stream_.attr("handle_int").cast<intptr_t>();
+		}
 
 		if (!idata.attr("dtype").cast<pybind11::dtype>().is(
 			odata.attr("dtype").cast<pybind11::dtype>()))
 			throw std::invalid_argument(
-                                "Input and output array must have the same type");
+				"Input and output array must have the same type");
 
-		const void* igpuarray = idata.attr("ptr").cast<const void*>();
-		void* ogpuarray = odata.attr("ptr").cast<void*>();
-		stream = (cudaStream_t)stream_.attr("handle_int").cast<intptr_t>();
+		const void* igpuarray = reinterpret_cast<const void*>(idata.attr("ptr").cast<intptr_t>());
+		void* ogpuarray = reinterpret_cast<void*>(odata.attr("ptr").cast<intptr_t>());
 
-                size_t sizeofType = idata.attr("itemsize").cast<size_t>();
+		size_t sizeofType = idata.attr("itemsize").cast<size_t>();
 		initStatus = cuttPlanMeasure(&plan, rank, reinterpret_cast<const int*>(&dim[0]),
 			reinterpret_cast<const int*>(&permutation[0]), sizeofType, stream,
 			igpuarray, ogpuarray, alpha, beta);
@@ -91,42 +143,79 @@ public :
 		cuttDestroy(plan);
 	}
 
-	cuttResult execute(const py::object& idata, py::object& odata, const void* alpha = NULL, const void* beta = NULL)
+	void execute(const py::object& idata, py::object& odata, const py::object& pyalpha, const py::object& pybeta)
 	{
 		py::object pycuda = py::module::import("pycuda");
-                py::object pygpuarray = pycuda.attr("gpuarray");
-                py::object pycudadriver = py::module::import("pycuda.driver");
-                py::object pystream = pycudadriver.attr("stream");
+		py::object pygpuarray = pycuda.attr("gpuarray").attr("GPUArray");
+		py::object pystream = pycuda.attr("driver").attr("Stream");
 
-                if (!py::isinstance(idata, pygpuarray))
-                        throw std::invalid_argument(
-                                "Input array must be a pycuda.gpuarray");
-                if (!py::isinstance(odata, pygpuarray))
-                        throw std::invalid_argument(
-                                "Output array must be a pycuda.gpuarray");
+		if (!py::isinstance(idata, pygpuarray))
+		{
+			std::stringstream ss;
+			ss << "Input array must be a pycuda.gpuarray, got: ";
+			ss << py::str(idata);
+			throw std::invalid_argument(ss.str());
+		}
+		if (!py::isinstance(odata, pygpuarray))
+		{
+			std::stringstream ss;
+			ss << "Output array must be a pycuda.gpuarray, got: ";
+			ss << py::str(odata);
+			throw std::invalid_argument(ss.str());
+		}
 
-                if (!idata.attr("dtype").cast<pybind11::dtype>().is(
-                        odata.attr("dtype").cast<pybind11::dtype>()))
-                        throw std::invalid_argument(
-                                "Input and output array must have the same type");
+		if (!idata.attr("dtype").cast<pybind11::dtype>().is(
+			odata.attr("dtype").cast<pybind11::dtype>()))
+		{
+			std::stringstream ss;
+			ss << "Input and output array must have the same type, got: ";
+			ss << py::str(idata.attr("dtype").cast<pybind11::dtype>());
+			ss << " and ";
+			ss << py::str(odata.attr("dtype").cast<pybind11::dtype>());
+			throw std::invalid_argument(ss.str());
+		}
 
-                const void* igpuarray = idata.attr("ptr").cast<const void*>();
-                void* ogpuarray = odata.attr("ptr").cast<void*>();
+		const void* igpuarray = reinterpret_cast<const void*>(idata.attr("ptr").cast<intptr_t>());
+		void* ogpuarray = reinterpret_cast<void*>(odata.attr("ptr").cast<intptr_t>());
 
-                size_t sizeofType = idata.attr("itemsize").cast<size_t>();
+		size_t sizeofType = idata.attr("itemsize").cast<size_t>();
 
-                if (!planInitialized)
-                {
+		if (!planInitialized)
+		{
 			// Now we know the sizeofType, and can initialize the plan handle.
-                        initStatus = cuttPlan(&plan, rank, reinterpret_cast<const int*>(&dim[0]),
-                                reinterpret_cast<const int*>(&permutation[0]), sizeofType, stream);
-                        planInitialized = true;
-                }
+			initStatus = cuttPlan(&plan, rank, reinterpret_cast<const int*>(&dim[0]),
+				reinterpret_cast<const int*>(&permutation[0]), sizeofType, stream);
+			planInitialized = true;
+		}
 
-                if (initStatus != CUTT_SUCCESS)
-                        return initStatus;
+		if (initStatus != CUTT_SUCCESS)
+		{
+			std::stringstream ss;
+			ss << "cuTT error: ";
+			ss << cuttErrorString(initStatus);
+			throw std::invalid_argument(ss.str());
+		}
 
-		return cuttExecute(plan, igpuarray, ogpuarray, alpha, beta);
+		double* alpha = NULL, valpha;
+		if (!pyalpha.is_none())
+		{
+			valpha = pyalpha.cast<double>();
+			alpha = &valpha;
+		}
+		double* beta = NULL, vbeta;
+		if (!pybeta.is_none())
+		{
+			vbeta = pybeta.cast<double>();
+			beta = &vbeta;
+		}
+		cuttResult status = cuttExecute(plan, igpuarray, ogpuarray, alpha, beta);
+		if (status != CUTT_SUCCESS)
+		{
+			std::stringstream ss;
+			ss << "cuTT error: ";
+			ss << cuttErrorString(status);
+			throw std::invalid_argument(ss.str());
+		}
 	}
 };
 
@@ -182,7 +271,7 @@ alpha             = scalar for input
 beta              = scalar for output
  
 Returns
-Success/unsuccess code)doc"
-		);
+Success/unsuccess code)doc",
+		py::arg().noconvert(), py::arg().noconvert(), py::arg("pyalpha") = py::none(), py::arg("pybeta") = py::none());
 }
 
