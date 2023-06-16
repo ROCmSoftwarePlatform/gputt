4adda84653f90b80123266e95fa3e903f16bb592
Dmitry Mikushin <dmitry@kernelgen.org>
2023-06-16 19:04:45 -0400

    Getting back the 4th dimension and original sizes in the example, testing both 4 and 8-byte data types

--- src/example/example.cu
+++ src/example/example.cu
@@ -1,9 +1,8 @@
 #include <gputt.h>
 #include <gputt_runtime.h>
 
+#include <iostream>
 #include <vector>
-
-using T = uint64_t;
 
 //
 // Error checking wrapper for gpuTT and vendor API.
@@ -12,7 +11,7 @@ using T = uint64_t;
 #define GPUTT_ERR_CHECK(stmt) do { \
   gputtResult err = stmt; \
   if (err != GPUTT_SUCCESS) { \
-    fprintf(stderr, "%s in file %s, function %s\n", #stmt,__FILE__,__FUNCTION__); \
+    fprintf(stderr, "Error \"%d\" at %s :%d\n", err, __FILE__,__LINE__); \
     exit(-1); \
   } \
 } while(0)
@@ -25,18 +24,16 @@ using T = uint64_t;
   } \
 } while (0)
 
-int main()
+template<typename T>
+void test()
 {
   // Four dimensional tensor
-  // Transpose (7, 11, 13) -> (13, 7, 11)
-  int dim[3] = {7, 11, 13};
-  int permutation[4] = {2, 0, 1};
-  int odim[3] = {13, 7, 11};
+  // Transpose (31, 549, 2, 3) -> (3, 31, 2, 549)
+  int dim[4] = {31, 549, 2, 3};
+  int permutation[4] = {3, 0, 2, 1};
 
-  std::vector<T> idata(dim[0] * dim[1] * dim[2]);
-  //std::generate(idata.begin(), idata.end(), rand);
-  for (int i = 0; i < idata.size(); i++)
-	  idata[i] = i;
+  std::vector<T> idata(dim[0] * dim[1] * dim[2] * dim[3]);
+  for (int i = 0; i < idata.size(); i++) idata[i] = i;
   std::vector<T> odata(idata.size());
 
   T* idataGPU;
@@ -48,10 +45,10 @@ int main()
 
   // Option 1: Create plan on NULL stream and choose implementation based on heuristics
   gputtHandle plan;
-  GPUTT_ERR_CHECK(gputtPlan(&plan, 3, dim, permutation, sizeof(idata[0]), 0));
+  GPUTT_ERR_CHECK(gputtPlan(&plan, 4, dim, permutation, sizeof(idata[0]), 0));
 
   // Option 2: Create plan on NULL stream and choose implementation based on performance measurements
-  // GPUTT_ERR_CHECK(gputtPlanMeasure(&plan, 3, dim, permutation, sizeof(idata[0]), 0, idata, odata));
+  // GPUTT_ERR_CHECK(gputtPlanMeasure(&plan, 4, dim, permutation, sizeof(idata[0]), 0, idata, odata));
 
   // Execute plan
   GPUTT_ERR_CHECK(gputtExecute(plan, idataGPU, odataGPU));
@@ -71,51 +68,37 @@ int main()
   for (int d0 = 0; d0 < dim[0]; d0++)
     for (int d1 = 0; d1 < dim[1]; d1++)
       for (int d2 = 0; d2 < dim[2]; d2++)
+        for (int d3 = 0; d3 < dim[3]; d3++)
         {
-          auto in = idata[d2 * dim[1] * dim[0] + d1 * dim[0] + d0];
-          auto& out2 = odata2[d1 * dim[0] * dim[2] + d0 * odim[2] + d2];
-	  // d1 * 7 * 13 + d0 * 13 + d2
-          // int dim[3] = {7, 11, 13};
-          // int permutation[4] = {2, 0, 1};
+          auto in = idata[d3 * dim[2] * dim[1] * dim[0] + d2 * dim[1] * dim[0] + d1 * dim[0] + d0];
+
+	  // int permutation[4] = {3, 0, 2, 1};
+          auto& out2 = odata2[d1 * dim[2] * dim[0] * dim[3] + d2 * dim[0] * dim[3] + d0 * dim[3] + d3];
 
           out2 = in;
 
-          if ((d0 == 2) && (d1 == 3) && (d2 == 5))
-          {
-	    for (int i = 0; i < odata.size(); i++)
-            {
-              if (out2 != odata[i]) continue;
-              
-	      printf("found equal to [%d][%d][%d] at %d!\n", d0, d1, d2, i);
-
-              // Find all x, y, z, w: d0 * x + d1 * y + d2 * z + d3 * w == i
-              for (int x = 1, xe = odata.size(); x < xe; x++)
-                for (int y = 1, ye = odata.size(); y < ye; y++)
-                  for (int z = 1, ze = odata.size(); z < ze; z++)
-                    {
-                      if ((x != 1) && (y != 1) && (z != 1)) continue;
-		      if ((x == y) || (y == z) || (x == z)) continue;
-                      //if (x * y * z > odata.size()) continue;
-
-                      int m = std::max(x, std::max(y, z));
-		      if ((m % x != 0) || (m % y != 0) || (m % z != 0)) continue; 
-
-		      //d0 * 13 + d1 * 13 * 7 + d2 * 1
-		      if ((d0 * x + d1 * y + d2 * z) == i)
-                        printf("Possible: %d %d %d\n", x, y, z);
-                    }
-            }
-          }
-#if 1
 	  // Compare with gpuTT's output element.
-          auto out = odata[d1 * dim[0] * dim[2] + d0 * dim[2] + d2];
+          auto out = odata[d1 * dim[2] * dim[0] * dim[3] + d2 * dim[0] * dim[3] + d0 * dim[3] + d3];
 	  if (out != out2)
 	  {
-            fprintf(stderr, "Output elements mismatch at [%d][%d][%d]: %lu != %lu\n", d0, d1, d2, out, out2);
+            std::cout << "Output elements mismatch at [" << d0 << "][" << d1 << "][" << d2 << "][" << d3 << "]: " <<
+              out << " != " << out2 << std::endl;
 	    exit(-1);
 	  }
-#endif
 	}
+
+  if (memcmp(odata.data(), odata2.data(), odata.size() * sizeof(odata[0])))
+  {
+    fprintf(stderr, "Output tensors mismatch\n");
+    exit(-1);
+  }
+}
+
+int main(int argc, char* argv[])
+{
+  // Using integer element type to ease elements comparison.
+  test<double>();
+  test<float>();
 
   return 0;
 }
